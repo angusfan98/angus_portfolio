3d9889e8370dbb38d6d7dea18acdcc502930b53d
Angus Fan <angusf@sfu.ca>
2023-04-05 15:11:41 -0700

    just need some small fixes

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
-import {BsLinkedin, BsGithub, BsTwitter} from 'react-icons/bs';
+import {BsLinkedin, BsGithub} from 'react-icons/bs';
 
 const SocialMedia = () => {
+  const style = { color: "black"}
+  const style_linkedin = {color:"#0366c3"}
   return (
     <div className='app__social'>
+      <a href="https://github.com/angusfan98">
         <div>
-            <BsTwitter/>
+            <BsGithub style={style}/>
         </div>
+      </a>
+      <a href="https://www.linkedin.com/in/angusfan98/">
         <div>
-            <BsGithub/>
+            <BsLinkedin style={style_linkedin}/>
         </div>
-        <div>
-            <BsLinkedin/>
-        </div>
-    </div>
+      </a>
+    </div> 
   )
 }
 
--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -69,7 +69,7 @@ const Work = () => {
             </div>
             <div className='app__work-content app__flex'>
               <h4 className='bold-text'>{work.title}</h4>
-              <p className="p-text" style={{marginTop:10}}>{work.description}</p>
+              <p className="p-text" style={{marginTop:10, textAlign:'center'}}>{work.description}</p>
 
               <div className='app__work-tag app__flex'>
                 <p className='p-text'>{work.tags[0]}</p>
